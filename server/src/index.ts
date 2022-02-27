85de73ebc142b81b28f597687917b700f92c3cb1
LeNhutHao <lenhuthao0777@gmail.com>
2022-02-27 15:02:42 +0700

    push first code

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+require('dotenv').config()
+import 'reflect-metadata'
+import express from 'express'
+import {createConnection} from 'typeorm'
+
+const main = async()=>{
+  await createConnection({
+    type: 'postgres',
+    database: 'reddit'
+  })
+}
